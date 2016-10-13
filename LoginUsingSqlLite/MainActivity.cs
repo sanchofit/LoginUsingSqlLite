8f1eda96f5ad957833027a2356d683af03fdcb28
sanchofit <sancho1903@gmail.com>
2016-10-13 13:36:06 +0300

    v1.0

---
+++ LoginUsingSqlLite/MainActivity.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using SQLite;
+
+namespace LoginUsingSqlLite
+{
+    [Activity(Label = "LoginUsingSqlLite", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        EditText txtusername;
+        EditText txtPassword;
+        Button btncreate;
+        Button btnsign;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            btnsign = FindViewById<Button>(Resource.Id.btnlogin);
+            btncreate = FindViewById<Button>(Resource.Id.btnregister);
+            txtusername = FindViewById<EditText>(Resource.Id.txtusername);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtpwd);
+            btnsign.Click += Btnsign_Click;
+            btncreate.Click += Btncreate_Click;
+            CreateDB();
+        }
+
+        private void Btncreate_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(RegisterActivity));
+        }
+
+        private void Btnsign_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Call Database  
+                var db = new SQLiteConnection(dpPath);
+                var data = db.Table<LoginTable>(); //Call Table  
+                var data1 = data.Where(x => x.username == txtusername.Text && x.password == txtPassword.Text).FirstOrDefault(); //Linq Query  
+                if (data1 != null)
+                {
+                    Toast.MakeText(this, "Login Success", ToastLength.Short).Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "Username or Password invalid", ToastLength.Short).Show();
+                }
+            }
+            catch (Exception ex)
+            {
+                Toast.MakeText(this, ex.ToString(), ToastLength.Short).Show();
+            }
+        }
+
+        public string CreateDB()
+        {
+            var output = "";
+            output += "Creating Databse if it doesnt exists";
+            string dpPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "user.db3"); //Create New Database  
+            var db = new SQLiteConnection(dpPath);
+            output += "\n Database Created....";
+            return output;
+        }
+
+    }
+}
+
